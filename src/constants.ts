fff490b2f9e73d91df149dd57beef4f898c5c48c
Linh Tran <52914988+linhlecle@users.noreply.github.com>
2020-01-14 15:21:29 +0700

    change endpoint (#10)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
-export const LOGIN_BUTTON_CLASS_NAME = process.env.LOGIN_BUTTON_CLASS_NAME || 'can-pass-login-button';
-export const CAN_WALLET_GRAPHQL_ENDPOINT = process.env.CAN_WALLET_GRAPHQL_ENDPOINT || '/wallet/graphql';
-export const CAN_PASS_SIGN_TX_URL = process.env.CAN_PASS_SIGN_TRANSACTION_URL || '/sign-transaction';
+export const LOGIN_BUTTON_CLASS_NAME =
+  process.env.LOGIN_BUTTON_CLASS_NAME || "can-pass-login-button";
+export const CAN_WALLET_GRAPHQL_ENDPOINT =
+  process.env.CAN_WALLET_GRAPHQL_ENDPOINT ||
+  "https://2jje08ikc2.execute-api.ap-southeast-1.amazonaws.com/dev/wallet";
+export const CAN_PASS_SIGN_TX_URL =
+  process.env.CAN_PASS_SIGN_TRANSACTION_URL ||
+  "http://pass.can.app.s3-website-ap-southeast-1.amazonaws.com/sign-transaction";
