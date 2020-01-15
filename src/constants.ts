b7182b5751c10b506c291538a5a4cdfa810989d1
ntpdung <dungnguyen@lecle.co.kr>
2020-01-15 09:20:48 +0700

    change sign transaction url

--- src/constants.ts
+++ src/constants.ts
@@ -5,4 +5,4 @@ export const CAN_WALLET_GRAPHQL_ENDPOINT =
   "https://2jje08ikc2.execute-api.ap-southeast-1.amazonaws.com/dev/wallet";
 export const CAN_PASS_SIGN_TX_URL =
   process.env.CAN_PASS_SIGN_TRANSACTION_URL ||
-  "http://pass.can.app.s3-website-ap-southeast-1.amazonaws.com/sign-transaction";
+  "http://test.can-pass.canfoundation.io/sign-transaction";
