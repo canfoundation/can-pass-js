69ef51188d743beadc6730c3ab894193b24a8289
Linh Tran <52914988+linhlecle@users.noreply.github.com>
2020-01-17 15:41:41 +0700

    Feat/update endpoint (#12)
    * change endpoint
    
    * change wallet endpoint
    
    * update endpoint in constants
    
    * update version to 0.9.11

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const LOGIN_BUTTON_CLASS_NAME =
   process.env.LOGIN_BUTTON_CLASS_NAME || "can-pass-login-button";
 export const CAN_WALLET_GRAPHQL_ENDPOINT =
   process.env.CAN_WALLET_GRAPHQL_ENDPOINT ||
-  "https://2jje08ikc2.execute-api.ap-southeast-1.amazonaws.com/dev/wallet";
+  "https://2jje08ikc2.execute-api.ap-southeast-1.amazonaws.com/dev/graphql";
 export const CAN_PASS_SIGN_TX_URL =
   process.env.CAN_PASS_SIGN_TRANSACTION_URL ||
   "http://test.can-pass.canfoundation.io/sign-transaction";
