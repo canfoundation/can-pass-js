fa1ae64201748e22bbe4ad82b7dcd8d2c76484cc
Duy Phan <duyphan@lecle.co.kr>
2020-03-03 11:10:11 +0700

    handle open async popup

--- src/ui.ts
+++ src/ui.ts
@@ -3,9 +3,13 @@
 import { CAN_PASS_SIGN_TX_URL } from "./constants";
 import logger from "./logger";
 
-const SIGN_TRANSACTION_MESSAGE_TYPE = 'sign-transaction';
+const SIGN_TRANSACTION_MESSAGE_TYPE = "sign-transaction";
+const CURRENT_WINDOW = "currentWindowRef";
 
-export const popup = (URL: string): Promise<any> => {
+const randomString = () =>
+  (+new Date() * Math.random()).toString(36).substring(0, 8);
+
+export const openPopup = (asyncPopup?: boolean, url?: string) => {
   if (typeof window === "undefined") throw new Error("Not Browser");
   const windowArea = {
     width: Math.floor(window.outerWidth * 0.8),
@@ -27,14 +31,36 @@ export const popup = (URL: string): Promise<any> => {
     window.screenY + (window.outerHeight - windowArea.height) / 8
   );
 
-  const sep = URL.indexOf("?") !== -1 ? "&" : "?";
-  const url = `${URL}${sep}`;
   const windowOpts = `toolbar=0,scrollbars=0,status=0,resizable=0,location=0,menuBar=0,
     width=${windowArea.width},height=${windowArea.height},
     left=${windowArea.left},top=${windowArea.top}`;
 
-  const authWindow = window.open(url, "producthuntPopup", windowOpts);
+  const authWindow = window.open(
+    url || CAN_PASS_SIGN_TX_URL,
+    "producthuntPopup",
+    windowOpts
+  );
+  if (!asyncPopup) {
+    const windowRef = randomString();
+    window[CURRENT_WINDOW] = windowRef;
+    window[windowRef] = authWindow;
+  }
 
+  return authWindow;
+};
+
+const popup = (URL: string): Promise<any> => {
+  const sep = URL.indexOf("?") !== -1 ? "&" : "?";
+  const url = `${URL}${sep}`;
+
+  let currentWindow = window[window[CURRENT_WINDOW]];
+  let authWindow;
+
+  if (currentWindow) {
+    currentWindow.location.href = url;
+  } else {
+    authWindow = openPopup(true, url);
+  }
   // Create IE + others compatible event handler
   const eventMethod = window.addEventListener
     ? "addEventListener"
@@ -45,6 +71,15 @@ export const popup = (URL: string): Promise<any> => {
   const eventer = window[eventMethod];
   const messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
 
+  const cleanUp = eventHandler => {
+    window[eventRemoveMethod](messageEvent, eventHandler, false);
+    if (currentWindow) {
+      currentWindow.close();
+      currentWindow = null;
+    }
+    if (authWindow) authWindow.close();
+  };
+
   return new Promise((resolve, reject) => {
     const eventHandler = e => {
       // if (e.origin !== window.SITE_DOMAIN) {
@@ -52,25 +87,19 @@ export const popup = (URL: string): Promise<any> => {
       //   reject(new Error('Not allowed'));
       // }
 
-      if(!e.data){
+      if (!e.data) {
         logger.debug("window opener return error", e);
-        window[eventRemoveMethod](messageEvent, eventHandler, false)
-        authWindow.close();
+        cleanUp(eventHandler);
         reject(new Error("No event data"));
       }
 
       if (e.data.type === SIGN_TRANSACTION_MESSAGE_TYPE) {
         logger.debug("window opener return message", e.data);
-        window[eventRemoveMethod](messageEvent, eventHandler, false)
-        authWindow.close();
+        cleanUp(eventHandler);
         resolve(e.data);
-      } 
-    }
-    eventer(
-      messageEvent,
-      eventHandler,
-      false
-    );
+      }
+    };
+    eventer(messageEvent, eventHandler, false);
   });
 };
 
@@ -80,18 +109,15 @@ export const signTx = (
   userName: string
 ): Promise<any> => {
   const url = `${CAN_PASS_SIGN_TX_URL}?txId=${txId}&userId=${userId}&userName=${userName}`;
-  return new Promise((resolve, reject) =>
-    popup(url)
-    .then(data => {
-      if (data.type === SIGN_TRANSACTION_MESSAGE_TYPE) {
-        if (data.error) {
-          reject(new Error(data.error));
-        }
-        
-        if (data.requestTxId === txId) {
-          resolve(data.tx);
-        }
+  return popup(url).then(data => {
+    if (data.type === SIGN_TRANSACTION_MESSAGE_TYPE) {
+      if (data.error) {
+        throw new Error(data.error);
       }
-    })
-  );
+
+      if (data.requestTxId === txId) {
+        return data.tx;
+      }
+    }
+  });
 };
