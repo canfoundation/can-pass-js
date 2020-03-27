c1794330c909c81a07649f8d761abe413bf25adf
Duy Phan <duyphan@lecle.co.kr>
2020-03-27 15:59:25 +0700

    update version

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const LOGIN_BUTTON_CLASS_NAME =
   process.env.LOGIN_BUTTON_CLASS_NAME || "can-pass-login-button";
 
 export const CAN_PASS_SIGN_TX_URL =
-  process.env.CAN_PASS_SIGN_TRANSACTION_URL || "http://localhost:3001/sign-transaction"
-  // "http://test.can-pass.canfoundation.io/sign-transaction";
+  process.env.CAN_PASS_SIGN_TRANSACTION_URL ||
+  "http://test.can-pass.canfoundation.io/sign-transaction";
