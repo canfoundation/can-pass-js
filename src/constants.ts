82c3b18e9d3d3a07e1a6466c74fbde1bd80b6bfc
dungntp <dungnguyen@lecle.co.kr>
2020-06-18 15:15:35 +0700

    remove unsed code #33

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ const LOGIN_BUTTON_CLASS_NAME =
   process.env.LOGIN_BUTTON_CLASS_NAME || "can-pass-login-button";
 
 const CAN_PASS_SIGN_TX_URL =
-  process.env.CAN_PASS_SIGN_TRANSACTION_URL ||
   "https://can-pass.canfoundation.io/sign-transaction";
 
 const CAN_KEYS_ENDPOINT = "https://prod.api.cryptobadge.app/can-keys/graphql";
