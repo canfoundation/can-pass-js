4381c356524f031b1f8d0c00da3b8b15da423e18
Duy Phan <duyphan@lecle.co.kr>
2020-07-01 14:42:09 +0700

    init

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,13 +1,47 @@
 /* global test, expect */
-import CanPass from '../index';
+import CanPass from "../index";
+import Storage, { LOCAL_STORAGE, MEMORY_STORAGE } from "../storage";
+import { global } from "../utils";
 
-CanPass.init({
-  appId: 'leonardo',
-  version: 'v1.0',
-  store: 'memory',
-  logger: console,
-});
+describe("Initialize CanPass", () => {
+  const info = {
+    clientId: "leonardo",
+    endPoint: "Hogwarts",
+    signTxURL: "Harry Potter",
+    version: "v1.0",
+    store: MEMORY_STORAGE,
+  };
 
-test('api', () => {
-  expect(CanPass.api()).toBe(0);
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  test("with memory", () => {
+    CanPass.init(info);
+
+    expect(Storage.read("clientId")).toEqual(info.clientId);
+    expect(Storage.read("endPoint")).toEqual(info.endPoint);
+    expect(Storage.read("signTxURL")).toEqual(info.signTxURL);
+    expect(Storage.read("version")).toEqual(info.version);
+  });
+
+  test("with browser localStorage", () => {
+    const window = {
+      localStorage: {
+        setItem: () => {},
+      },
+    };
+
+    jest.doMock("../utils", () => {
+      return { global: 'abc' };
+    });
+
+    expect(global).toEqual("");
+    CanPass.init({ ...info, store: LOCAL_STORAGE });
+
+    expect(Storage.read("clientId")).toEqual(info.clientId);
+    expect(Storage.read("endPoint")).toEqual(info.endPoint);
+    expect(Storage.read("signTxURL")).toEqual(info.signTxURL);
+    expect(Storage.read("version")).toEqual(info.version);
+  });
 });
