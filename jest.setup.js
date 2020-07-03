6e7c10908aed4f90a056ead6ea31916766a8c8eb
Duy Phan <phanduy344@gmail.com>
2020-07-03 11:12:28 +0700

    Setup first testing (#38)
    * done testing of canpass init
    
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

---
+++ jest.setup.js
@@ -0,0 +1,1 @@
+Object.defineProperty(Window, 'localStorage', { setItem: () => {} });
--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,13 +1,35 @@
-/* global test, expect */
-import CanPass from '../index';
+/**
+ * @jest-environment jsdom
+ */
 
-CanPass.init({
-  appId: 'leonardo',
-  version: 'v1.0',
-  store: 'memory',
-  logger: console,
-});
+import CanPass from "../index";
+import Storage, { LOCAL_STORAGE, MEMORY_STORAGE } from "../storage";
 
-test('api', () => {
-  expect(CanPass.api()).toBe(0);
+describe("Initialize CanPass", () => {
+  const info = {
+    clientId: "leonardo",
+    endPoint: "Hogwarts",
+    signTxURL: "Harry Potter",
+    version: "v1.0",
+    store: MEMORY_STORAGE,
+  };
+
+  const assertResults = () => {
+    expect(Storage.read("clientId")).toEqual(info.clientId);
+    expect(Storage.read("endPoint")).toEqual(info.endPoint);
+    expect(Storage.read("signTxURL")).toEqual(info.signTxURL);
+    expect(Storage.read("version")).toEqual(info.version);
+  }
+
+  it("with memory", () => {
+    CanPass.init(info);
+
+    assertResults();
+  });
+
+  it("with browser localStorage", () => {
+    CanPass.init({ ...info, store: LOCAL_STORAGE });
+    
+    assertResults();
+  });
 });
